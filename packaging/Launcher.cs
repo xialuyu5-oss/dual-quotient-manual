418c250cb23c1fd8c553195bb8e6c53b9c531029
RootSeeker <xialuyu5@gmail.com>
2026-09-20 10:47:13 +0800

    Validate workbook backups and update Windows launcher to v0.4.0

--- packaging/Launcher.cs
+++ packaging/Launcher.cs
@@ -10,8 +10,8 @@ using System.Threading;
 using System.Windows.Forms;
 
 [assembly: AssemblyTitle("双商训练手册")]
-[assembly: AssemblyVersion("0.3.0.0")]
-[assembly: AssemblyFileVersion("0.3.0.0")]
+[assembly: AssemblyVersion("0.4.0.0")]
+[assembly: AssemblyFileVersion("0.4.0.0")]
 
 internal static class Launcher
 {
